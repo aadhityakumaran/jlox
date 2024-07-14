25222edebf40989a4079c7f48dfa0f8a2e5e4da9
aadhityakumaran <kumaranpoke@gmail.com>
2024-07-14 22:18:56 +0530

    Integrated parser to Lox (AstPrinter is only used to test parser, it won't be maintained)

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -44,9 +44,10 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token: tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(parser.parse()));
     }
 
     static void error(int line, String message) {
