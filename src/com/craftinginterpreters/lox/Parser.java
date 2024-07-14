2f60098965e4f32f240a42b220fcec6a17ede0c8
aadhityakumaran <kumaranpoke@gmail.com>
2024-07-14 22:03:39 +0530

    Parser for expressions

---
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,117 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Parser {
+    private static class ParseError extends RuntimeException {}
+
+    private final List<Token> tokens;
+    private int current = 0;
+
+    Parser(List<Token> tokens) {
+        this.tokens = tokens;
+    }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        return parseBinary(this::comparison, BANG_EQUAL, EQUAL_EQUAL);
+    }
+
+    private Expr comparison() {
+        return parseBinary(this::term, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL);
+    }
+
+    private Expr term() {
+        return parseBinary(this::factor, PLUS, MINUS);
+    }
+
+    private Expr factor() {
+        return parseBinary(this::unary, STAR, SLASH);
+    }
+
+    private Expr unary() {
+        if (match(BANG, MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) return new Expr.Literal(previous().literal);
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+
+        throw error(peek(), "Expect expression.");
+    }
+
+    private Expr parseBinary(Supplier<Expr> eval, TokenType... operators) {
+        Expr expr = eval.get();
+
+        while (match(operators)) {
+            Token operator = previous();
+            Expr right = eval.get();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+
+    private boolean match(TokenType ...types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+        throw error(peek(), message);
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+}
